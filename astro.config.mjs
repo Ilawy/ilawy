a78e962b155bbfeead4e8fd52d713dbd39157590
ilawy <mm.ilawy@gmail.com>
2025-01-25 21:55:58 +0200

    feat: added whyframe setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react'
+import { whyframe } from '@whyframe/core'
+import { whyframeAstro } from '@whyframe/astro'
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()],
+  vite: {
+    plugins: [
+      whyframe({
+        defaultSrc: "/frames/default"
+      }),
+      whyframeAstro({
+        defaultFramework: 'react'
+      })
+    ]
+  }
 });
---
+++ src/components/gallery/hello.tsx
@@ -0,0 +1,7 @@
+
+export default function Hello() {
+    const word = "hello"
+    return <p>
+        This is a demo for writing small components in whyframe
+    </p>
+}
